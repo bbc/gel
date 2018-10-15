110aeadd36ca22e7005569e9bead1194bed898b4
Heydon Pickering <heydon@heydonworks.com>
2018-10-15 17:43:11 -0400

    nopte and important shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,30 @@ module.exports = function (eleventyConfig) {
   var pluginTOC = require('eleventy-plugin-toc');
   var hljs = require('highlight.js');
 
-  var md = require('markdown-it')({
+  var shortcodes = {
+    note: {
+      render: function (attrs) {
+        return `
+          <aside class="note" aria-label="Note:">
+            <p class="note_label" aria-hidden="true"><strong>Note</strong></p>
+            <p>${md.render(attrs.text)}</p>
+          </aside>
+        `;
+      }
+    },
+    important: {
+      render: function (attrs) {
+        return `
+          <aside class="note" aria-label="Important:">
+            <p class="note_label" aria-hidden="true"><strong>Important</strong></p>
+            <p>${md.render(attrs.text)}</p>
+          </aside>
+        `;
+      }
+    }
+  }
+
+  const md = require('markdown-it')({
     html: true,
     breaks: true,
     linkify: true,
@@ -17,7 +40,7 @@ module.exports = function (eleventyConfig) {
 
       return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
     }
-  }).use(require('markdown-it-anchor'));
+  }).use(require('markdown-it-anchor')).use(require('markdown-it-shortcode-tag'), shortcodes);
 
   eleventyConfig.addPlugin(pluginTOC);
   eleventyConfig.setLibrary("md", md);
