799513aa4046eadabeb564bd7afa6efb47e80c2f
Heydon Pickering <heydon@heydonworks.com>
2018-10-19 11:48:39 -0400

    standalone launcher

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
   var shortcodes = {
     mark: {
       render: function (attrs) {
-        var char = (attrs.is === 'good')? '✓' : (attrs.is === 'bad')? '✕' : '?';
+        var char = (attrs.is === 'good') ? '✓' : (attrs.is === 'bad') ? '✕' : '?';
         return `
           <div class="circular circular__${attrs.is}" aria-hidden="true">${char}</div>
         `;
@@ -32,37 +32,37 @@ module.exports = function (eleventyConfig) {
       return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
     }
   })
-  .use(require('markdown-it-anchor'))
-  .use(require('markdown-it-shortcode-tag'), shortcodes)
-  .use(require('markdown-it-footnote'))
-  .use(require('markdown-it-container'), 'breakout', {
-    validate: function(params) {
-      return params.trim().match(/^(info|help|alert)/);
-    },
-    render: function (tokens, idx) {
-      var m = tokens[idx].info.trim().match(/^(info|help|alert) (.+)$/);
+    .use(require('markdown-it-anchor'))
+    .use(require('markdown-it-shortcode-tag'), shortcodes)
+    .use(require('markdown-it-footnote'))
+    .use(require('markdown-it-container'), 'breakout', {
+      validate: function (params) {
+        return params.trim().match(/^(info|help|alert)/);
+      },
+      render: function (tokens, idx) {
+        var m = tokens[idx].info.trim().match(/^(info|help|alert) (.+)$/);
 
-      if (tokens[idx].nesting === 1) { // opening tag
-        return `
+        if (tokens[idx].nesting === 1) { // opening tag
+          return `
           <div class="gel-breakout-box gel-breakout-box extra-padding">
             <aside aria-label="${m[2]}">
               <h4 aria-hidden="true"><svg class="gel-breakout-box__icon gel-icon gel-icon--text"><use xlink:href="${data.site.basedir}static/images/gel-icons-core-set.svg#gel-icon-${m[1]}" style="fill:#404040;"></use></svg>${m[2]}</h4><div>
               <p>`;
-      } else { // closing tag
-        return `</p>
+        } else { // closing tag
+          return `</p>
             </aside>
           </div>
           `;
+        }
       }
-    }
-  });
+    });
 
-  eleventyConfig.addPlugin(function(eleventyConfig, pluginNamespace) {
+  eleventyConfig.addPlugin(function (eleventyConfig, pluginNamespace) {
     eleventyConfig.namespace(pluginNamespace, () => {
-      eleventyConfig.addFilter('toc', function(content, opts){
+      eleventyConfig.addFilter('toc', function (content, opts) {
         var $ = cheerio.load(content);
         var result = '<ol id="gel-toc__links" class="gel-toc">';
-        $('h2').each(function(i, h2) {
+        $('h2').each(function (i, h2) {
           result += '<li><a href="#' + h2.attribs.id + '">' + $(h2).text() + '</a></li>';
         });
         return result + '</ol>';
@@ -71,6 +71,7 @@ module.exports = function (eleventyConfig) {
   });
   eleventyConfig.setLibrary("md", md);
   eleventyConfig.addPassthroughCopy("src/static/css");
+  eleventyConfig.addPassthroughCopy("src/static/html");
   eleventyConfig.addPassthroughCopy("src/static/css/bbc-grandstand/dist");
   eleventyConfig.addPassthroughCopy("src/static/images");
   eleventyConfig.addPassthroughCopy("src/static/js");
--- docs/static/js/demo.js
+++ docs/static/js/demo.js
@@ -1,21 +1,25 @@
-class Demo extends HTMLElement {
-  constructor() {
-    super();
-    this.code = this.querySelector('template').content.cloneNode(true);
-    const random = + new Date();
-    this.uniq = 'demo-' + random;
+if (window.customElements.define && document.head.attachShadow) {
+  class Demo extends HTMLElement {
+    constructor() {
+      super();
+      this.code = this.querySelector('template').content.cloneNode(true);
+      const random = + new Date();
+      this.uniq = 'demo-' + random;
 
-    this.js = this.code.querySelector('script');
-    this.jsText = this.js.textContent;
-    this.js.textContent = `(function() { const demo = document.getElementById('${this.uniq}').shadowRoot; ${this.jsText} })();`;
-    this.wrapper = document.createElement('div');
-    this.wrapper.classList.add('live-demo');
-    this.wrapper.id = this.uniq;
-    this.wrapper.style.all = 'initial';
-    this.wrapper.attachShadow({ mode: 'open' });
-    this.wrapper.shadowRoot.appendChild(document.importNode(this.code, true));
-    this.appendChild(this.wrapper);
+      this.js = this.code.querySelector('script');
+      if (this.js) {
+        this.jsText = this.js.textContent;
+        this.js.textContent = `(function() { const demo = document.getElementById('${this.uniq}').shadowRoot; ${this.jsText} })();`;
+      }
+      this.wrapper = document.createElement('div');
+      this.wrapper.classList.add('live-demo');
+      this.wrapper.id = this.uniq;
+      this.wrapper.style.all = 'initial';
+      this.wrapper.attachShadow({ mode: 'open' });
+      this.wrapper.shadowRoot.appendChild(document.importNode(this.code, true));
+      this.appendChild(this.wrapper);
+    }
   }
-}
 
-customElements.define('live-demo', Demo);
+  customElements.define('live-demo', Demo);
+}
