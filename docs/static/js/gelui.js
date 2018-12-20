f5aead38417960c52302f4cc325b6feedf66fef5
Heydon Pickering <heydon@heydonworks.com>
2018-12-20 15:01:22 +0000

    rebuild

--- docs/static/js/gelui.js
+++ docs/static/js/gelui.js
@@ -1,1 +1,209 @@
-!function(){var e=window.gelui||{};(e.BreakoutBox={}).init=function(){},window.gelui||(window.gelui=e)}(),function(){var e=window.gelui||{};(e.Carousel={}).init=function(){var e;"inert"in HTMLElement.prototype||(Object.defineProperty(HTMLElement.prototype,"inert",{enumerable:!0,get:function(){return this.hasAttribute("inert")},set:function(e){e?this.setAttribute("inert",""):this.removeAttribute("inert")}}),window.addEventListener("load",function(){function i(e){for(;e&&e!==document.documentElement;){if(e.hasAttribute("inert"))return e;e=e.parentElement}return null}function n(e){var t=e.path;return t&&t[0]||e.target}function o(e){e.path[e.path.length-1]!==window&&(r(n(e)),e.preventDefault(),e.stopPropagation())}function r(e){var t=i(e);if(t){if(document.hasFocus()&&0!==u){var n=(d||document).activeElement;if(function(t){var n=null;try{n=new KeyboardEvent("keydown",{keyCode:9,which:9,key:"Tab",code:"Tab",keyIdentifier:"U+0009",shiftKey:!!t,bubbles:!0})}catch(e){try{(n=document.createEvent("KeyboardEvent")).initKeyboardEvent("keydown",!0,!0,window,"Tab",0,t?"Shift":"",!1,"en")}catch(e){}}if(n){try{Object.defineProperty(n,"keyCode",{value:9})}catch(e){}document.dispatchEvent(n)}}(u<0),n!=(d||document).activeElement)return;var o,r=document.createTreeWalker(document.body,NodeFilter.SHOW_ELEMENT,{acceptNode:function(e){return!e||!e.focus||e.tabIndex<0?NodeFilter.FILTER_SKIP:t.contains(e)?NodeFilter.FILTER_REJECT:NodeFilter.FILTER_ACCEPT}});for(r.currentNode=t,r=(-1===Math.sign(u)?r.previousNode:r.nextNode).bind(r);o=r();)if(o.focus(),(d||document).activeElement!==n)return}e.blur()}}var e,t;e="/*[inert]*/*[inert]{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none}",(t=document.createElement("style")).type="text/css",t.styleSheet?t.styleSheet.cssText=e:t.appendChild(document.createTextNode(e)),document.body.appendChild(t);var c=function(e){return null};window.ShadowRoot&&(c=function(e){for(;e&&e!==document.documentElement;){if(e instanceof window.ShadowRoot)return e;e=e.parentNode}return null});var u=0;document.addEventListener("keydown",function(e){u=9===e.keyCode?e.shiftKey?-1:1:0}),document.addEventListener("mousedown",function(e){u=0});var d=null;document.body.addEventListener("focus",function(e){var t=n(e);if((e=t==e.target?null:c(t))!=d){if(d){if(!(d instanceof window.ShadowRoot))throw Error("not shadow root: "+d);d.removeEventListener("focusin",o,!0)}e&&e.addEventListener("focusin",o,!0),d=e}r(t)},!0),document.addEventListener("click",function(e){i(n(e))&&(e.preventDefault(),e.stopPropagation())},!0)})),e=document.querySelectorAll(".gel-carousel"),Array.prototype.forEach.call(e,function(e){var t,n=e.querySelector(".gel-carousel-scrollable"),o=e.querySelector(".gel-carousel-list"),r=o.children,i=o.offsetWidth/2,c=e.querySelector(".gel-carousel-prev"),u=e.querySelector(".gel-carousel-next");function d(){c.disabled=n.scrollLeft<1,u.disabled=n.scrollLeft===o.scrollWidth-o.offsetWidth}if(c.disabled=!0,c.addEventListener("click",function(e){n.scrollLeft+=-i}),u.addEventListener("click",function(e){n.scrollLeft+=i}),n.addEventListener("scroll",function(){window.clearTimeout(t),t=setTimeout(d,200)}),"IntersectionObserver"in window){var a=new IntersectionObserver(function(e,t){Array.prototype.forEach.call(e,function(e){e.isIntersecting?e.target.removeAttribute("inert"):e.target.setAttribute("inert","inert")})},{root:n,threshold:.5});Array.prototype.forEach.call(r,function(e){a.observe(e)})}})},window.gelui||(window.gelui=e)}(),function(){var e=window.gelui||{};(e.Promo={}).init=function(){var e=document.querySelectorAll(".gel-promo");Array.prototype.forEach.call(e,function(e){var t=e.querySelector(".gel-promo-image"),n=e.querySelector(".gel-promo-headline a");t&&n&&(t.style.cursor="pointer",t.addEventListener("click",function(){n.click()}))})},window.gelui||(window.gelui=e)}(),function(){var e=window.gelui||{};(e.Tabs={}).init=function(){var e=document.querySelectorAll(".gel-tabs");Array.prototype.forEach.call(e,function(e){var o=e.querySelector("ul"),r=o.querySelectorAll("a"),i=e.querySelectorAll("section[id]"),c=function(){var e,t;return window.location.hash&&(t=Array.prototype.indexOf.call(r,document.getElementById("tab-"+window.location.hash.substring(1)))),e=!(void 0===t||t<0)||(t=0,!1),{index:t,focus:e}},u=function(e,t){void 0!==e&&-1<e&&(r[e].removeAttribute("aria-current"),i[e].hidden=!0),r[t.index].setAttribute("aria-current","true"),i[t.index].hidden=!1,void 0!==e&&t.focus&&i[t.index].focus()};Array.prototype.forEach.call(r,function(e,t){e.id="tab-"+e.getAttribute("href").substring(1);var n=i[t];n.setAttribute("tabindex","-1"),n.setAttribute("aria-labelledby",e.id),n.hidden=!0}),window.addEventListener("hashchange",function(e){var t=o.querySelector("[aria-current]"),n=t?Array.prototype.indexOf.call(r,t):void 0;u(n,c())},!1),window.addEventListener("DOMContentLoaded",function(){u(void 0,c())})})},window.gelui||(window.gelui=e)}();
+/**
+ * BreakoutBox
+ * @namespace gelui
+ * @method gelui.BreakoutBox.init
+ */
+
+(function() {
+  var g = window.gelui || {};
+  var self = g.BreakoutBox = {};
+
+  self.init = function() {
+    // nothing to initialise
+  }
+
+  if (!window.gelui) { window.gelui = g; }
+})();/**
+ * Carousel
+ * @namespace gelui
+ * @method gelui.Carousel.init
+ */
+
+(function() {
+  var g = window.gelui || {};
+  var self = g.Carousel = {};
+  
+  self.init = function() {
+    /* inert attribute polyfill, from https://github.com/GoogleChrome/inert-polyfill */
+    "inert" in HTMLElement.prototype || (Object.defineProperty(HTMLElement.prototype, "inert", { enumerable: !0, get: function () { return this.hasAttribute("inert") }, set: function (h) { h ? this.setAttribute("inert", "") : this.removeAttribute("inert") } }), window.addEventListener("load", function () {
+      function h(a) {
+        var b = null; try { b = new KeyboardEvent("keydown", { keyCode: 9, which: 9, key: "Tab", code: "Tab", keyIdentifier: "U+0009", shiftKey: !!a, bubbles: !0 }) } catch (g) {
+          try {
+            b = document.createEvent("KeyboardEvent"), b.initKeyboardEvent("keydown",
+              !0, !0, window, "Tab", 0, a ? "Shift" : "", !1, "en")
+          } catch (d) { }
+        } if (b) { try { Object.defineProperty(b, "keyCode", { value: 9 }) } catch (g) { } document.dispatchEvent(b) }
+      } function k(a) { for (; a && a !== document.documentElement;) { if (a.hasAttribute("inert")) return a; a = a.parentElement } return null } function e(a) { var b = a.path; return b && b[0] || a.target } function l(a) { a.path[a.path.length - 1] !== window && (m(e(a)), a.preventDefault(), a.stopPropagation()) } function m(a) {
+        var b = k(a); if (b) {
+          if (document.hasFocus() && 0 !== f) {
+            var g = (c || document).activeElement;
+            h(0 > f ? !0 : !1); if (g != (c || document).activeElement) return; var d = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT, { acceptNode: function (a) { return !a || !a.focus || 0 > a.tabIndex ? NodeFilter.FILTER_SKIP : b.contains(a) ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT } }); d.currentNode = b; d = (-1 === Math.sign(f) ? d.previousNode : d.nextNode).bind(d); for (var e; e = d();)if (e.focus(), (c || document).activeElement !== g) return
+          } a.blur()
+        }
+      } (function (a) {
+        var b = document.createElement("style"); b.type = "text/css"; b.styleSheet ?
+          b.styleSheet.cssText = a : b.appendChild(document.createTextNode(a)); document.body.appendChild(b)
+      })("/*[inert]*/*[inert]{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none}"); var n = function (a) { return null }; window.ShadowRoot && (n = function (a) { for (; a && a !== document.documentElement;) { if (a instanceof window.ShadowRoot) return a; a = a.parentNode } return null }); var f = 0; document.addEventListener("keydown", function (a) { f = 9 === a.keyCode ? a.shiftKey ? -1 : 1 : 0 }); document.addEventListener("mousedown",
+        function (a) { f = 0 }); var c = null; document.body.addEventListener("focus", function (a) { var b = e(a); a = b == a.target ? null : n(b); if (a != c) { if (c) { if (!(c instanceof window.ShadowRoot)) throw Error("not shadow root: " + c); c.removeEventListener("focusin", l, !0) } a && a.addEventListener("focusin", l, !0); c = a } m(b) }, !0); document.addEventListener("click", function (a) { var b = e(a); k(b) && (a.preventDefault(), a.stopPropagation()) }, !0)
+    }));
+
+    (function () {
+      var cards = document.querySelectorAll('.gel-carousel');
+      Array.prototype.forEach.call(cards, function (carousel) {
+        var scrollable = carousel.querySelector('.gel-carousel-scrollable');
+        var list = carousel.querySelector('.gel-carousel-list');
+        var items = list.children;
+        var scrollAmount = list.offsetWidth / 2;
+        var prev = carousel.querySelector('.gel-carousel-prev');
+        var next = carousel.querySelector('.gel-carousel-next');
+
+        prev.disabled = true;
+
+        prev.addEventListener('click', function (e) {
+          scrollable.scrollLeft += -scrollAmount;
+        });
+        next.addEventListener('click', function (e) {
+          scrollable.scrollLeft += scrollAmount;
+        });
+
+        function disableEnable() {
+          prev.disabled = scrollable.scrollLeft < 1;
+          next.disabled = scrollable.scrollLeft === list.scrollWidth - list.offsetWidth;
+        }
+
+        // Debounce the button disabling function on scroll
+        var debounced;
+        scrollable.addEventListener('scroll', function () {
+          window.clearTimeout(debounced);
+          debounced = setTimeout(disableEnable, 200);
+        });
+
+        if ('IntersectionObserver' in window) {
+          var observerSettings = {
+            root: scrollable,
+            threshold: 0.5
+          }
+
+          var callback = function (items, observer) {
+            Array.prototype.forEach.call(items, function (item) {
+              if (item.isIntersecting) {
+                item.target.removeAttribute('inert');
+              } else {
+                item.target.setAttribute('inert', 'inert');
+              }
+            });
+          }
+
+          var observer = new IntersectionObserver(callback, observerSettings);
+          Array.prototype.forEach.call(items, function (item) {
+            observer.observe(item);
+          });
+        }
+      });
+    })();
+  }
+
+  if (!window.gelui) { window.gelui = g; }
+})();/**
+ * Promo
+ * @namespace gelui
+ * @method gelui.Promo.init - Adds click behaviour to the image element in gel-promo components.
+ */
+
+(function() {
+  var g = window.gelui || {};
+  var self = g.Promo = {};
+  
+  self.init = function() {
+    var promos = document.querySelectorAll('.gel-promo');
+    
+    Array.prototype.forEach.call(promos, function (promo) {
+      var img = promo.querySelector('.gel-promo-image');
+      var link = promo.querySelector('.gel-promo-headline a');
+
+      if (img && link) {
+        img.style.cursor = 'pointer';
+        img.addEventListener('click', function () {
+          link.click();
+        });
+      }
+    });
+  }
+
+  if (!window.gelui) { window.gelui = g; }
+})();/**
+ * Tabs
+ * @namespace gelui
+ * @method gelui.Tabs.init
+ */
+
+(function() {
+  var g = window.gelui || {};
+  var self = g.Tabs = {};
+  
+  self.init = function() {
+    var tabInterfaces = document.querySelectorAll('.gel-tabs');
+    Array.prototype.forEach.call(tabInterfaces, function (tabInterface) {
+      var tablist = tabInterface.querySelector('ul');
+      var tabs = tablist.querySelectorAll('a');
+      var panels = tabInterface.querySelectorAll('section[id]');
+
+      var tabInfo = function () {
+        var focus;
+        var tab;
+        if (window.location.hash) {
+          tab = Array.prototype.indexOf.call(
+            tabs,
+            document.getElementById('tab-' + window.location.hash.substring(1))
+          );
+        }
+        if (typeof tab === 'undefined' || tab < 0) {
+          tab = 0;
+          focus = false;
+        }
+        else {
+          focus = true;
+        }
+        return {
+          index: tab,
+          focus: focus
+        };
+      }
+
+      var switchTab = function (oldIndex, tabInfo) {
+        if (typeof oldIndex !== 'undefined' && oldIndex > -1) {
+          tabs[oldIndex].removeAttribute('aria-current');
+          panels[oldIndex].hidden = true;
+        }
+
+        tabs[tabInfo.index].setAttribute('aria-current', 'true');
+        panels[tabInfo.index].hidden = false;
+
+        if (typeof oldIndex !== 'undefined' && tabInfo.focus) {
+          panels[tabInfo.index].focus();
+        }
+      }
+
+      Array.prototype.forEach.call(tabs, function (tab, i) {
+        tab.id = 'tab-' + tab.getAttribute('href').substring(1);
+
+        var panel = panels[i];
+        panel.setAttribute('tabindex', '-1');
+        panel.setAttribute('aria-labelledby', tab.id);
+        panel.hidden = true;
+      });
+
+      window.addEventListener('hashchange', function (e) {
+        var selected = tablist.querySelector('[aria-current]');
+        var oldIndex = selected ? Array.prototype.indexOf.call(tabs, selected) : undefined;
+        switchTab(oldIndex, tabInfo());
+      }, false);
+
+      window.addEventListener('DOMContentLoaded', function () {
+        switchTab(undefined, tabInfo());
+      });
+    });
+  }
+
+  if (!window.gelui) { window.gelui = g; }
+})();
