b4fdaf49ef51d44a8c411fbc33af551a409d943a
Heydon Pickering <heydon@heydonworks.com>
2019-06-11 18:29:37 +0100

    draft 1 and split demos

--- src/_includes/js/components/gef-search.js
+++ src/_includes/js/components/gef-search.js
@@ -10,9 +10,26 @@
 
   self.init = function () { }
 
-  self.constructor = function (elem, options) {
+  self.toggle = function (button, search) {
+    search.hidden = true;
+    button.hidden = false;
+
+    var input = search.querySelector('input');
+    var closeButton = search.querySelector('.gef-search-close');
+    button.addEventListener('click', function () {
+      search.hidden = false;
+      input.focus();
+    });
+
+    closeButton.addEventListener('click', function () {
+      search.hidden = true;
+      button.focus();
+    });
+  }
+
+  self.constructor = function (elem, data, buildFunction, options) {
     var settings = {
-      debounce: 500
+      debounce: 250
     };
 
     // Overwrite defaults where they are provided in options
@@ -22,16 +39,49 @@
       }
     }
 
+    this.data = data;
     this.input = elem.querySelector('input');
     this.suggestions = elem.querySelector('.gef-search-suggestions');
+    this.suggestions.hidden = false;
+    this.suggestionsLinks = this.suggestions.querySelector('.gef-search-suggestions-links');
     this.suggestionsLabel = this.suggestions.querySelector('.gef-search-suggestions-label');
+
+    this.suggestions.style.height = '0';
+
+    this.hideSuggestions = function () {
+      var height = this.suggestions.scrollHeight;
+      var elTransition = this.suggestions.style.transition;
+      this.suggestions.style.transition = '';
+
+      requestAnimationFrame(function () {
+        this.suggestions.style.height = height + 'px';
+        this.suggestions.style.transition = elTransition;
+
+        requestAnimationFrame(function () {
+          this.suggestions.style.height = 0 + 'px';
+        }.bind(this));
+      }.bind(this));
+
+      this.suggestions.setAttribute('aria-hidden', 'true');
+    }
+
+    this.showSuggestions = function () {
+      var height = this.suggestions.scrollHeight;
+
+      this.suggestions.style.height = height + 'px';
+    }
 
     this.suggest = function (input) {
       if (input.value === '') {
-        this.suggestions.hidden = true;
+        this.suggestions.setAttribute('aria-hidden', 'true');
+        this.hideSuggestions();
         return;
+      } else {
+        buildFunction(this);
+        this.suggestions.setAttribute('aria-hidden', 'false');
+        this.suggestionsLabel.innerHTML = 'We have <span class="search-suggestion-count">' + this.suggestionsLinks.querySelector('a').length + '</span> suggestions for you<span class="gef-sr">, please find them below</span>:'
+        this.showSuggestions();
       }
-
     }
 
     var debounced;
@@ -46,5 +96,4 @@
       }
     }.bind(this));
   }
-
 })();
